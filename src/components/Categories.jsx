2d12e002e5f82e2c7f435827f134cfedab41fce4
kozincov <kozincov1998@gmail.com>
2022-08-02 19:40:01 +0300

    added useState&onClick for categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
 const Categories = () => {
+
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+    const [activeIndex, setActiveIndex] = React.useState(0)
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index)
+    }
+
     return (
 
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {categories.map((value, index) => {
+                    return <li onClick={() => onClickCategory(index)}
+                               className={activeIndex === index ? 'active' : ''}>{value}</li>
+                })}
             </ul>
         </div>
     );
