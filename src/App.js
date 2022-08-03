3a97360807f185f93dc79f92fe92b716acb5accc
kozincov <kozincov1998@gmail.com>
2022-08-04 02:29:57 +0300

    get pizzas with backend, used useEffect

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import React from "react";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-import pizzas from './assets/pizzas.json'
+
 
 function App() {
+    const [items, setItems] = React.useState([])
+
+    React.useEffect(() => {
+        fetch('https://62eb00a2705264f263d20b10.mockapi.io/items')
+            .then((res)=>{
+                return res.json()
+            })
+            .then((arr)=>{
+                setItems(arr)
+            })
+    }, [])
+
     return (
         <div className="wrapper">
             <Header/>
@@ -18,7 +30,7 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map((obj) => (<PizzaBlock key={obj.id} {...obj}/>))}
+                        {items.map((obj) => (<PizzaBlock key={obj.id} {...obj}/>))}
                     </div>
                 </div>
             </div>
