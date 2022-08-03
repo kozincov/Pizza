e54fcf380131595f6d28b1c0d3c25fe6218ecc2f
kozincov <kozincov1998@gmail.com>
2022-08-03 18:45:24 +0300

    rendering array pizzas

--- src/App.js
+++ src/App.js
@@ -4,28 +4,26 @@ import React from "react";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
+import pizzas from './assets/pizzas.json'
 
 function App() {
-  return (
-      <div className="wrapper">
-        <Header/>
-        <div className="content">
-          <div className="container">
-            <div className="content__top">
-              <Categories/>
-              <Sort/>
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories/>
+                        <Sort/>
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        {pizzas.map((obj) => (<PizzaBlock {...obj}/>))}
+                    </div>
+                </div>
             </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-              <PizzaBlock title={'Мексиканская-пицца'} price={20}/>
-              <PizzaBlock title={'Мексиканская-пицца'} price={20}/>
-              <PizzaBlock title={'Мексиканская-пицца'} price={20}/>
-              <PizzaBlock title={'Мексиканская-пицца'} price={20}/>
-            </div>
-          </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;
