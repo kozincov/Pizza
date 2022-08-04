ae5014d38cb0a3b6eff4e42c73edfd45766a10db
kozincov <kozincov1998@gmail.com>
2022-08-04 19:29:51 +0300

    added new components, Routes, Route

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
 import './scss/app.scss';
 import Header from "./components/Header";
 import React from "react";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
-import {Skeleton} from "./components/PizzaBlock/Skeleton";
+import {Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
 
 
 function App() {
-    const [items, setItems] = React.useState([])
-    const [isLoading, setIsLoading] = React.useState(true)
-
-    React.useEffect(() => {
-        fetch('https://62eb00a2705264f263d20b10.mockapi.io/items')
-            .then((res) => {
-                return res.json()
-            })
-            .then((arr) => {
-                setItems(arr)
-                setIsLoading(false)
-            })
-    }, [])
 
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
                 <div className="container">
-                    <div className="content__top">
-                        <Categories/>
-                        <Sort/>
-                    </div>
-                    <h2 className="content__title">Все пиццы</h2>
-                    <div className="content__items">
-                        {isLoading
-                            ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
-                            : items.map((obj) => (<PizzaBlock key={obj.id} {...obj}/>))}
-                    </div>
+                    <Routes>
+                        <Route path={'/Pizza'} element={<Home/>}  />
+                        <Route path={'/cart'} element={<Cart/>}  />
+                        <Route path={'*'} element={<NotFound/>}  />
+                    </Routes>
                 </div>
             </div>
         </div>
